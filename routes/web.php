9a069bdb9e1c13952351f644d7db644df6e81d5b
chumaceraearlecc <chumaceraearlecc@gmail.com>
2026-08-15 15:48:53 +0800

    feat: add company routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyController;
 
-Route::get('/', [CompanyController::class, 'home']);
-Route::get('/about', [CompanyController::class, 'about']);
-Route::get('/services', [CompanyController::class, 'services']);
-Route::get('/contact', [CompanyController::class, 'contact']);
+Route::get('/', [CompanyController::class, 'home'])->name('home');
+Route::get('/about', [CompanyController::class, 'about'])->name('about');
+Route::get('/services', [CompanyController::class, 'services'])->name('services');
+Route::get('/contact', [CompanyController::class, 'contact'])->name('contact');
